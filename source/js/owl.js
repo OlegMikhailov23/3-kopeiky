75aab7bd322a7c5c6ed68c4136e9d11940b70505
Oleg Mikhaylov <dkfolkin@gmail.com>
2020-06-09 21:49:08 +0300

    final

--- source/js/owl.js
+++ source/js/owl.js
@@ -1,30 +1,66 @@
-
-$(document).ready(function(){
-$('.gallery-slide').owlCarousel({
-        loop:true,
-        dots:false,
-        items: 2,
-        autoplay: true,
+$(document).ready(function () {
+  $('.gallery-slide').owlCarousel({
+    loop: true,
+    dots: false,
+    items: 2,
+    autoplay: true,
+    nav: true,
+    autoplaySpeed: 1000,
+    autoplayTimeout: 10000,
+    mouseDrag: true,
+    touchDrag: true,
+    margin: 0,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false
+      },
+      700: {
+        items: 3,
         nav: true,
-        autoplaySpeed: 1000,
-        autoplayTimeout: 10000,
-        mouseDrag: true,
+        dots: false,
+        autoplay: true,
         touchDrag: true,
-        margin: 0,
-        responsive:{
-            0:{
-                items:1,
-                dots: false
-            },
-            700:{
-                items:3,
-                nav: true,
-                dots: false,
-                autoplay: true,
-                touchDrag: true,
-                mouseDrag: true,
-            }
-        }
-    });
+        mouseDrag: true,
+      }
+    }
+  });
+
+
+  $('.price-slide').owlCarousel({
+    loop: true,
+    dots: false,
+    items: 2,
+    autoplay: true,
+    nav: true,
+    autoplaySpeed: 1500,
+    autoplayTimeout: 15000,
+    mouseDrag: true,
+    touchDrag: true,
+    margin: 0,
+    responsive: {
+      0: {
+        items: 1,
+        dots: false
+      },
+      768: {
+        items: 2.3,
+        nav: true,
+        dots: false,
+        autoplay: true,
+        touchDrag: true,
+        mouseDrag: true,
+      },
+      1140: {
+        items: 2,
+        nav: true,
+        dots: false,
+        autoplay: false,
+        touchDrag: false,
+        mouseDrag: false,
+        items: 3
+      }
+    }
+  });
 
 });
--- source/js/send-ajax.js
+++ source/js/send-ajax.js
@@ -1,10 +1,15 @@
 $(function () {
   var ESC_KEYCODE = 27;
   var modalBg = document.querySelector('.modal-background');
+  var modalForm = document.querySelector('#modal-form');
   var successMessage = document.querySelector('#success-box');
   var errorMessage = document.querySelector('#error-box');
   var closeBtnSucces = document.querySelector('#succes-close-btn');
   var closeBtnError = document.querySelector('#error-close-btn');
+  var closeBtn = document.querySelector('#form-close-btn');
+  var modalFormBtn = document.querySelector('#recall-btn');
+  var loader = document.querySelector('.load');
+  var userMessage = document.querySelector('#modal-form-usermessage');
 
 
   var closeSucces = function () {
@@ -24,17 +29,40 @@ $(function () {
   var onEscPress = function (evt) {
     if (evt.keyCode === ESC_KEYCODE) {
       closeSucces();
+      closeForm();
     }
   };
 
   var closeIffield = function (evt) {
     if (evt.target === modalBg) {
       closeSucces();
+      closeForm();
     }
+  };
+
+  var openForm = function () {
+    popup.classList.add('modal-content-show');
+    modalBg.style.display = 'block';
+    document.addEventListener('keydown', onEscPress);
+    document.addEventListener('mouseup', closeIffield);
+  }
+
+  var closeForm = function () {
+    popup.classList.add('modal-content-hide');
+    modalBg.style.display = 'none';
+    document.removeEventListener('keydown', onEscPress);
+    document.removeEventListener('mouseup', closeIffield);
+    setTimeout(function () {
+      popup.classList.remove('modal-content-show');
+
+      popup.classList.remove('modal-content-hide');
+    }, 250);
   };
 
   closeBtnSucces.addEventListener('click', closeSucces);
   closeBtnError.addEventListener('click', closeSucces);
+  modalFormBtn.addEventListener('click', openForm);
+  closeBtn.addEventListener('click', closeForm);
 
 
   $('#contacts__form').submit(function (e) {
@@ -54,6 +82,43 @@ $(function () {
         document.addEventListener('mouseup', closeIffield);
         document.addEventListener('keydown', onEscPress);
         $("#error-box").css('display', 'block') && $(".modal-background").css('display', 'block');
+      }
+    });
+  });
+
+  $('#modal-form').submit(function (e) {
+    e.preventDefault();
+    var formdata = 'name=' + $('#modal-name').val() + '&phone=' + $('#modal-phone').val();
+    //запишем все данные формы в переменную data
+    $.ajax({
+      url: 'send.php',
+      data: formdata,
+      type: 'post',
+      success: function (respond) {
+        loader.style.display = 'block';
+        setTimeout(function () {
+          loader.style.display = 'none';
+        }, 800);
+        setTimeout(function () {
+          userMessage.innerText = 'Ваша заявка принята! Мы свяжемся с вами в течении 15 минут!';
+        }, 800);
+        setTimeout(function () {
+          userMessage.innerText = ' ';
+          modalForm.reset();
+        }, 8000);
+      },
+      error: function () {
+        loader.style.display = 'block';
+        setTimeout(function () {
+          loader.style.display = 'none';
+        }, 800);
+        setTimeout(function () {
+          userMessage.innerText = 'Что-то пошло не так, повторите попытку позже.';
+        }, 800);
+        setTimeout(function () {
+          userMessage.innerText = ' ';
+          modalForm.reset();
+        }, 8000);
       }
     });
   });
